0ff9b760b74218792da438c51350bbf30d5970b5
Rodrigo Faria <rodrigo.p.faria98@gmail.com>
2019-06-20 00:10:28 -0300

    correçao do construtor inline

--- ligacaoSimples.h
+++ ligacaoSimples.h
@@ -11,9 +11,8 @@ class LigacaoSimples : public Ligacao
 private:
     double _numTelefone;
 public:
-    LigacaoSimples(double dur, double num, Date d) : _duracao(dur), _data(d), _numTelefone(num); {};
+    LigacaoSimples(double dur, double num, Date d) : _duracao(dur), _data(d), _numTelefone(num) {};
     double get_numTelefone() { return this->numTelefone; }
 };
 
 #endif // LIGACAO_SIMPLES_H
-
